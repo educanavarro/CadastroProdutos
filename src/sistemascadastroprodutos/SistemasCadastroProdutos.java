f297728bef9e847ac2c3622f04fb4f1b71474d68
educanavarro <educanavarro@users.noreply.github.com>
2019-09-28 00:09:05 -0300

    Iniciando Projeto

---
+++ src/sistemascadastroprodutos/SistemasCadastroProdutos.java
@@ -0,0 +1,12 @@
+package sistemascadastroprodutos;
+
+import telas.TelaPrincipal;
+
+public class SistemasCadastroProdutos {
+
+    public static void main(String[] args) {
+        System.out.println("Iniciando Sistema...");
+        new TelaPrincipal().setVisible(true);
+    }
+    
+}
